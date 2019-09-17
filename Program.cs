c1ec1fa07f5cce1934923fa30aea889a6f6f2970
Jason Nowicki <jason.nowicki@ky.gov>
2019-09-17 14:04:46 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Select an email promotion 1 or 0");
+
+            var emailPromotion = Console.ReadLine();
+
+            SqlConnection conn = new SqlConnection("data source=localhost;database=AdventureWorks2017;integrated security=true");
+            SqlCommand comm = new SqlCommand("Select FirstName, MiddleName, LastName from Person.Person where emailpromotion = " + emailPromotion, conn);
+
+            conn.Open();
+            SqlDataReader dr = comm.ExecuteReader(CommandBehavior.CloseConnection);
+
+            while (dr.Read())
+            {
+                Console.WriteLine(GetFullName(dr));
+            }
+
+            Console.ReadLine();
+        }
+
+
+        /// <summary>
+        /// Gets the full name.
+        /// </summary>
+        /// <param name="dr">The dr.</param>
+        /// <returns>System.String.</returns>
+        private static string GetFullName(SqlDataReader dr)
+        {
+            return dr.GetValue(dr.GetOrdinal("FirstName")).ToString();
+        }
+    }
+}
